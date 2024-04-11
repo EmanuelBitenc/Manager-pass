7c038400005b297c20e1392ae4c5cbf4984b3cad
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2024-04-10 22:35:02 -0300

    Mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ParticipantesList from "./components/ParticipantesList/participantesList"
 
 const App = () => {
   return (
-    <div className="max-w-[1216px] mx-auto py-5 bg-zinc-900 text-gray-200 ">
+    <div className="max-w-[1216px] min-w-[1216px] mx-auto py-5 bg-zinc-900 text-gray-200 ">
       <Header />
       <ParticipantesList />
     </div>
